fc1dc66b372dd9f6ea54712e2d421b9443ad6329
Brieuc QUERTIER <bquertier@cegid.com>
2022-01-19 12:00:42 +0100

    dockerization done

---
+++ dockerfile
@@ -0,0 +1,11 @@
+FROM node:lts-alpine
+RUN mkdir /app
+WORKDIR /app
+COPY package*.json ./
+RUN npm i
+
+COPY tsconfig.json ./
+COPY src src
+COPY public public
+
+CMD npm start 
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import InscriptionPage from "./components/InscriptionPage/InscriptionPage";
+import { Button } from '@mui/material';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('Button', ()=>{
+    it('renders a button', ()=>{
+        render(<Button />)
+        expect(screen.getByRole('button')).toBeInTheDocument()
+    })
+})
+
+describe('When user clicks sign up button', () => {
+  it('renders Thank you!', () => {
+    render(<Button />)
+    fireEvent.click(screen.getByRole('button'))
+    // expect(screen.getByText(/Thank you!/i)).toBeInTheDocument()
+  })
+})
